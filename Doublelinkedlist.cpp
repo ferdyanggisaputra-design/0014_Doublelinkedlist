1aadde81393334dfae0a250773a491c2d98cb52a
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-30 17:10:28 +0700

    Menambahkan metode searchData untuk mencari record berdasarkan nomor roll

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -182,4 +182,18 @@ public:
         }
     }
 
-    
+    void searchData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+
+        
