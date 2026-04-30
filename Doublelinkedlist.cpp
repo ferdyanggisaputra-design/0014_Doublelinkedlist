0b76e9f9f4eb92a0740d0b0960a0efa4b5854f45
ferdyanggisaputra-design <ferdyanggisaputra@gmail.com>
2026-04-30 17:07:31 +0700

    Menambahkan logika untuk menghapus node dari DoubleLinkedList dan menampilkan pesan konfirmasi setelah penghapusan

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -123,4 +123,9 @@ public:
                 current->next->prev = current->prev;
         }
 
-       
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
+    }
+
+    
